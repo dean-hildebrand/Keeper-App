c9406df488799f8c4f461c89872065e3525d52d8
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-08-08 12:52:27 -0400

    Rendered current year dynamically as a footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react"
 
+let date = new Date()
+let currentYear = date.getFullYear()
+
 function Footer(){
-    
+    return(
+        <p>Copyright @ {currentYear}</p>
+    )
 }
 
 export default Footer
